d88547bd1e5f46e2ef823155f3709d130a93a7cc
KimSeokWon <kswamen@naver.com>
2021-02-05 21:52:37 +0900

    210205

--- board/src/main/java/com/board/controller/BoardController.java
+++ board/src/main/java/com/board/controller/BoardController.java
@@ -8,23 +8,105 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.board.dao.BoardDAO;
 import com.board.domain.BoardVO;
+import com.board.domain.Page;
 import com.board.service.BoardService;
 
 @Controller
 @RequestMapping("/board/*")
 public class BoardController {
+	 @Inject
+	 private BoardService service;
+	
+	 @RequestMapping(value = "/list", method = RequestMethod.GET)
+	 public void getList(Model model) throws Exception {
+	  
+	  List<BoardVO> list = null;
+	  list = service.list();
+	  model.addAttribute("list", list);
+	 }
+	 
+	 @RequestMapping(value = "/write", method = RequestMethod.GET)
+	 public void getWrite() throws Exception {
+		 
+	 }
+	 
+	 @RequestMapping(value = "/write", method = RequestMethod.POST)
+	 public String postWrite(BoardVO vo) throws Exception {
+		 service.write(vo);
+		 return "redirect:/board/list";
+	 }
+	 
+	 @RequestMapping(value = "/view", method = RequestMethod.GET)
+	 public void getView(@RequestParam("bno") int bno, Model model) throws Exception{
+		 BoardVO vo = service.view(bno);
+		 model.addAttribute("view", vo);
+	 }
+	 
+	// 게시물 수정
+	 @RequestMapping(value = "/modify", method = RequestMethod.GET)
+	 public void getModify(@RequestParam("bno") int bno, Model model) throws Exception {
+		 BoardVO vo = service.view(bno);
+		 model.addAttribute("view", vo);
+	 }
+	 
+	// 게시물 수정
+	 @RequestMapping(value = "/modify", method = RequestMethod.POST)
+	 public String postModify(BoardVO vo) throws Exception {
+		 service.modify(vo);
+		 return "redirect:/board/view?bno=" + vo.getBno();
+	 }
+	 
+	// 게시물 삭제
+	 @RequestMapping(value = "/delete", method = RequestMethod.GET)
+	 public String getDelete(@RequestParam("bno") int bno) throws Exception {
+		 service.delete(bno);  
+		 return "redirect:/board/list";
+	 }
+	 
+	// 게시물 목록 + 페이징 추가
+	 @RequestMapping(value = "/listPage", method = RequestMethod.GET)
+	 public void getListPage(Model model, @RequestParam("num") int num) throws Exception {
+		 Page page = new Page();
+		 
+		 page.setNum(num);
+		 page.setCount(service.count());  
 
- @Inject
- private BoardService service;
+		 List<BoardVO> list = null; 
+		 list = service.listPage(page.getDisplayPost(), page.getPostNum());
 
- @RequestMapping(value = "/list", method = RequestMethod.GET)
- public void getList(Model model) throws Exception {
-  
-  List<BoardVO> list = null;
-  list = service.list();
-  model.addAttribute("list", list);
- }
+		 model.addAttribute("list", list);   
+		 model.addAttribute("page", page);
+		 model.addAttribute("select", num);
+	 }
+	 
+	// 게시물 목록 + 페이징 추가 + 검색
+	 @RequestMapping(value = "/listPageSearch", method = RequestMethod.GET)
+	 public void getListPageSearch(Model model, @RequestParam("num") int num, 
+	   @RequestParam(value = "searchType", required = false, defaultValue = "title") String searchType, 
+	   @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword
+	   ) throws Exception {
+		  Page page = new Page();
+		  
+		  page.setNum(num);
+		  //page.setCount(service.count());  
+		  page.setCount(service.searchCount(searchType, keyword));
+		  // 검색 타입과 검색어
+		  //page.setSearchTypeKeyword(searchType, keyword);
+		  page.setSearchType(searchType);
+		  page.setKeyword(keyword);
+		  
+		  List<BoardVO> list = null; 
+		  //list = service.listPage(page.getDisplayPost(), page.getPostNum());
+		  list = service.listPageSearch(page.getDisplayPost(), page.getPostNum(), searchType, keyword);
+		  
+		  model.addAttribute("list", list);
+		  model.addAttribute("page", page);
+		  model.addAttribute("select", num);
+	  
+		  //model.addAttribute("searchType", searchType);
+		  //model.addAttribute("keyword", keyword);
+	 }
 }
